37600961a9e370560d19529f5fa83e27c3056ce3
jdcr1425 <juandavidcr7@gmail.com>
2020-05-09 17:49:07 -0500

    enviroment variable MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV ==='dev'){
     urlDB = `mongodb://localhost:27017/cafe`;
 }else{
-    urlDB =`mongodb+srv://jd1425:jdcr1425@cluster0-v0gkf.mongodb.net/cafe?retryWrites=true&w=majority`;
+    urlDB =process.env.MONGO_URI;
 }
 
 process.env.URLDB= urlDB;
